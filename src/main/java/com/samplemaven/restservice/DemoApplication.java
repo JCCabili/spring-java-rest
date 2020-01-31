34dd9b32904fed49f7a222f92940d059b2132f1a
JCCabili <cabilijc@gmail.com>
2020-01-31 15:03:24 +0800

    This is update 1

--- src/main/java/com/samplemaven/restservice/DemoApplication.java
+++ src/main/java/com/samplemaven/restservice/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 	@GetMapping(value = "/world")
 	public TestModel getHelloWorlde() {
 		TestModel testModel = new TestModel();
-		testModel.name = "JC Cabili Update Online Web Hooks";
+		testModel.name = "JC Cabili Update";
 		testModel.order = 1;
 
 		return testModel;
